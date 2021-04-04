d317a10940d37cddc1453df673473dd529b6be57
Zhaolong Liu <liuzhaolong4@gmail.com>
2021-04-04 17:03:47 -0400

    update

---
+++ nav.js
@@ -0,0 +1,14 @@
+/*$(document).ready(function(event) {
+    $("#navBar").load("navBar.html");
+});*/
+/*$.get("navBar.html", function(data){
+    $(".navBar").replaceWith(data);
+});*/
+document.write('<div class="navBar">\
+    <link href="css/navBar.css" rel="stylesheet" />\
+    <a class="active" href="index.html">Home</a>\
+    <!--<a href="#contact">Contact</a>-->\
+    <a href="login.html">Log-in</a>\
+    <a href="#about">Sign-up</a>\
+    </div>'
+);
