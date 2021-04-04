9e9c7701d776fbfdc94bfb270cf6b33b02c967af
Zhaolong Liu <zhaolongliu@umass.edu>
2021-04-04 16:32:23 -0400

    update todo list

---
+++ todo.js
@@ -0,0 +1,18 @@
+$(document).ready(function(event) {
+    $('#todoList li span').click(function(event) {
+        $(this).parent().remove();        
+    });
+
+    $("#addTask").click(function (event){
+        $(".todoDiv").css("display","block");
+    });
+
+    $(".addBtn").click(function (event){
+        let title = $("#myInput").val();
+        $("#todoList").append('<li><span class="delete"></span>'+title+'</li>');
+    });
+
+    $(".cancelBtn").click(function (event){
+        $(".todoDiv").css("display","none");
+    });
+});
