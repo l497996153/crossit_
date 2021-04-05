d6527e53efe9b6cd30771c2e0bd1c6b9da087bb1
Zhaolong Liu <liuzhaolong4@gmail.com>
2021-04-05 01:03:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 app
+  .use(express.static(path.join(__dirname, 'public')))
   .post("/login", async (req, res) => {
     const username = (req.query.username) ? req.query.username : "";
     const password = (req.query.password) ? req.query.password : "";
