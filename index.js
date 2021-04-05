a5fb3d222f7fbe40d45cadfb385121ceb5cad355
Zhaolong Liu <liuzhaolong4@gmail.com>
2021-04-05 16:06:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express(); 
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+  }
+});
 
 app
   .use(express.static(path.join(__dirname, 'public')))
