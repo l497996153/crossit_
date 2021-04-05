dc120f6d3982a18ab2ece16ba9a4e00d738ec3ef
Zhaolong Liu <liuzhaolong4@gmail.com>
2021-04-05 16:10:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require("express");
 const app = express(); 
 const path = require("path");
 const PORT = process.env.PORT || 5000;
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-      rejectUnauthorized: false
-  }
-});
 
 app
   .use(express.static(path.join(__dirname, 'public')))
@@ -18,8 +12,8 @@ app
     res.sendFile(path.join(__dirname + '/index.html'));
   })
   .post("/login", async (req, res) => {
-    const username = (req.query.username) ? req.query.username : "";
-    const password = (req.query.password) ? req.query.password : "";
+    const username = req.body.username;
+    const password = req.body.password;
     console.log(username + " " +password);
     if (validateLogin(username, password)) {
       if(username=="admin" && password =="pass"){
