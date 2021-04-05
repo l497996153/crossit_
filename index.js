b48c736bdd6fd4fe072944c4c14b7870bf590289
Zhaolong Liu <liuzhaolong4@gmail.com>
2021-04-05 02:05:06 -0400

    fix

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 app
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'JS')))
   .use(express.urlencoded({extended: true}))
   .set('views', path.join(__dirname, 'views'))
   .post("/login", async (req, res) => {
