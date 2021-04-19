9242ec3ed699093fff863d40e80201865fe13e7e
Zhaolong Liu <liuzhaolong4@gmail.com>
2021-04-19 15:02:00 -0400

    db connection

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express(); 
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 app
   .use(express.static(path.join(__dirname, 'public')))
