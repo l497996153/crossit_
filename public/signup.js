92342fb96c28cc89a338da40a4892c8a8f2b3e37
Zhaolong Liu <liuzhaolong4@gmail.com>
2021-04-21 18:42:48 -0400

    debug

--- public/signup.js
+++ public/signup.js
@@ -1,10 +1,10 @@
 $(document).ready(function (event) {
-  $(".signupbtn").click(function (event) {
+  $("#signupbtn").click(function (event) {
     let pass = $("#password").val();
     let confirm = $("#co-password").val();
+    alert(pass);
+    alert(confirm);
     if (pass != confirm) {
-      alert(pass);
-      alert(confirm);
       event.preventDefault();
       $("#errmsg").text("Password does not match");
     }
