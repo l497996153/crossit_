dfec372330876c49bb3af3232ba0d4c94470d3ee
luckas72 <luckas727015957yj@gmail.com>
2021-05-03 16:26:13 -0400

    cleanup

--- public/todo.js
+++ public/todo.js
@@ -2,37 +2,40 @@ $(document).ready(function (event) {
   getAllTodos();
   function getAllTodos() {
     let id = $("#user_id").text();
-    $.get("/api/todos/"+id, function(todos) {
-        let $list = $("#todoList");
-        $list.html("");
-        console.log(todos);
-        if(todos.length > 0){
-          todos.forEach(function(todo) {
-              $list.append('<li><span class="delete"></span>' + 
-              todo.remind + '</li>')
-          });
-          $("#todoList li span").click(function (event) {
-            console.log("span: "+$(this).parent().text());
-            const todo = {
-              id: $("#user_id").text(),
-              remind: $(this).parent().text(),
-            };
-            $.ajax({
-              type: "DELETE",
-              url: "/api/todos",
-              data: JSON.stringify(todo),
-              contentType: "application/json"
-            }).done(function(data) {
+    $.get("/api/todos/" + id, function (todos) {
+      let $list = $("#todoList");
+      $list.html("");
+      console.log(todos);
+      if (todos.length > 0) {
+        todos.forEach(function (todo) {
+          $list.append(
+            '<li><span class="delete"></span>' + todo.remind + "</li>"
+          );
+        });
+        $("#todoList li span").click(function (event) {
+          console.log("span: " + $(this).parent().text());
+          const todo = {
+            id: $("#user_id").text(),
+            remind: $(this).parent().text(),
+          };
+          $.ajax({
+            type: "DELETE",
+            url: "/api/todos",
+            data: JSON.stringify(todo),
+            contentType: "application/json",
+          })
+            .done(function (data) {
               // Successfully deleted entree
               getAllTodos();
-            }).fail(function(jqXHR) {
-              console.log("error")
+            })
+            .fail(function (jqXHR) {
+              console.log("error");
             });
-          });
-        }
+        });
+      }
     });
   }
-  
+
   $("#menuButton").click(function (event) {
     if ($(".dropdown-menu").is(":visible")) {
       $(".dropdown-menu").css("display", "none");
@@ -64,15 +67,17 @@ $(document).ready(function (event) {
       type: "POST",
       url: "/api/todos",
       data: JSON.stringify(todo),
-      contentType: "application/json"
-    }).done(function(data) {
-      // Reset the form after saving the entree
-      $("#myInput").html("");
-      getAllTodos();
-    }).fail(function(jqXHR) {
-      console.log("error")
-      //$("#error").html("The todo could not be added.");
-    });
+      contentType: "application/json",
+    })
+      .done(function (data) {
+        // Reset the form after saving the entree
+        $("#myInput").html("");
+        getAllTodos();
+      })
+      .fail(function (jqXHR) {
+        console.log("error");
+        //$("#error").html("The todo could not be added.");
+      });
   });
 
   $(".cancelBtn").click(function (event) {
